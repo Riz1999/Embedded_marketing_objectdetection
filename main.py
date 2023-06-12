10aa581fec2a841975ac3ee629ceb149761bf093
Riz1999 <rizwan.zhad@gmail.com>
2023-06-12 11:16:31 +0530

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>> stage{STAGE_NAME} started <<<<")
+    data_ingestion= DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>stage{STAGE_NAME} Completed <<<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+
+
+
